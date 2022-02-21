5d8fcc80b259a1c33b65050fc2f77545a70371c5
Rishav Bhowmik <bhowmikrishav@gmail.com>
2022-02-21 10:36:09 +0530

    cargo fmt

--- logchain/src/lib.rs
+++ logchain/src/lib.rs
@@ -198,7 +198,7 @@ mod tests {
 
     #[test]
     fn test_make_chunks() {
-        let data: [u8;16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+        let data: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
         let chunk_size = 10;
         let (blocks_required, mut chunks) = make_chunks(&data, chunk_size);
         assert_eq!(blocks_required, 2);
@@ -207,14 +207,14 @@ mod tests {
         assert_eq!(chunks.next().unwrap(), &[11, 12, 13, 14, 15, 16]);
         assert_eq!(chunks.next(), None);
 
-        let data: [u8;0] = [];
+        let data: [u8; 0] = [];
         let chunk_size = 1;
         let (blocks_required, mut chunks) = make_chunks(&data, chunk_size);
         assert_eq!(blocks_required, 0);
         assert_eq!(chunks.clone().count(), 0);
         assert_eq!(chunks.next(), None);
 
-        let data: [u8;1] = [1];
+        let data: [u8; 1] = [1];
         let chunk_size = 1;
         let (blocks_required, mut chunks) = make_chunks(&data, chunk_size);
         assert_eq!(blocks_required, 1);
@@ -222,7 +222,7 @@ mod tests {
         assert_eq!(chunks.next().unwrap(), &[1]);
         assert_eq!(chunks.next(), None);
 
-        let data: [u8;2] = [1, 2];
+        let data: [u8; 2] = [1, 2];
         let chunk_size = 1;
         let (blocks_required, mut chunks) = make_chunks(&data, chunk_size);
         assert_eq!(blocks_required, 2);
@@ -231,7 +231,7 @@ mod tests {
         assert_eq!(chunks.next().unwrap(), &[2]);
         assert_eq!(chunks.next(), None);
 
-        let data: [u8;3] = [1, 2, 3];
+        let data: [u8; 3] = [1, 2, 3];
         let chunk_size = 1;
         let (blocks_required, mut chunks) = make_chunks(&data, chunk_size);
         assert_eq!(blocks_required, 3);
