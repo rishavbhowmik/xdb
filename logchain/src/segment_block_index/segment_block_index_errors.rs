8a792502210670bb82f389d5bd7ec13bfa30aa42
Rishav Bhowmik <bhowmikrishav@gmail.com>
2022-03-20 20:44:23 +0530

    block_index_from_buffer_insufficient_buffer_size fix

--- logchain/src/segment_block_index/segment_block_index_errors.rs
+++ logchain/src/segment_block_index/segment_block_index_errors.rs
@@ -2,10 +2,10 @@ use util::error::{Error, ErrorType};
 
 pub fn block_index_from_buffer_insufficient_buffer_size(buffer_size: usize) -> Error {
     Error::new(
-        ErrorType::Happens,
+        ErrorType::Critical,
         "block_index_from_buffer_insufficient_buffer_size",
         Some(format!(
-            "Insufficient buffer size:\n\t Buffer size: {}",
+            "Possible logical error or storage corrupt: Insufficient buffer size:\n\t Buffer size: {}",
             buffer_size
         )),
     )
