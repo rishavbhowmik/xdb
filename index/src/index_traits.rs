bab4533d124c842ef95c9cbea075c966ebe0c3e7
Rishav Bhowmik <bhowmikrishav@gmail.com>
2022-03-31 10:37:56 +0530

    index_trait patch - use copied bytes as value

--- index/src/index_traits.rs
+++ index/src/index_traits.rs
@@ -2,9 +2,9 @@ use util::error::Error;
 
 pub trait IndexTrait<K, V> {
     /// Get values associated with the given key.
-    fn get(&self, key: &K) -> Vec<V>;
+    fn get(&self, key: K) -> Vec<V>;
     /// Check if value exists against the given key.
-    fn exists(&self, key: &K, value: &V) -> bool;
+    fn exists(&self, key: K, value: V) -> bool;
 
     /// Add new value to the index against the given key.
     /// Returns tuple for syncing.
@@ -12,18 +12,18 @@ pub trait IndexTrait<K, V> {
 
     /// Remove value from the index against the given key.
     /// Returns tuple for syncing.
-    fn remove(&mut self, key: &K, value: V) -> Result<Vec<u8>, Error>;
+    fn remove(&mut self, key: K, value: V) -> Result<Vec<u8>, Error>;
 
     /// Delete the key from the index.
     /// Returns tuple for syncing.
-    fn delete(&mut self, key: &K) -> Result<Vec<u8>, Error>;
+    fn delete(&mut self, key: K) -> Result<Vec<u8>, Error>;
 }
 
 pub trait UniqueIndexTrait<K, V> {
     /// Get values associated with the given key.
-    fn get(&self, key: &K) -> Option<V>;
+    fn get(&self, key: K) -> Option<V>;
     /// Check if value exists against the given key.
-    fn exists(&self, key: &K, value: &V) -> bool;
+    fn exists(&self, key: K, value: V) -> bool;
 
     /// Set unique value for the given key.
     /// - if overwrite: true - overwrites existing value if any.
@@ -33,5 +33,5 @@ pub trait UniqueIndexTrait<K, V> {
 
     /// Delete the key from the index.
     /// Returns tuple for syncing.
-    fn delete(&mut self, key: &K) -> Result<Vec<u8>, Error>;
+    fn delete(&mut self, key: K) -> Result<Vec<u8>, Error>;
 }
